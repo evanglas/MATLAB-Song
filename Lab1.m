b8f8ab3416c0e88c1c1f4add228b80cb7b7faab3
Evan Glas <43274966+eglas-20@users.noreply.github.com>
2021-10-07 15:40:09 -0400

    Final Song

---
+++ Lab1.m
@@ -0,0 +1,333 @@
+% Piano treble part note vector
+
+pianoTreble = [pin("B", "pp", "s", 3) pin("D", "pp", "s", 4) pin("B", "pp", "s", 4) pin("G", "pp", "s", 4)... % 1
+    pin("B", "pp", "s", 3) pin("D", "pp", "s", 4) pin("B", "pp", "s", 4) pin("G", "pp", "s", 4)...
+    pin("B", "pp", "s", 3) pin("D", "pp", "s", 4) pin("B", "pp", "s", 4) pin("G", "pp", "s", 4)...
+    pin("B", "pp", "s", 3) pin("D", "pp", "s", 4) pin("B", "pp", "s", 4) pin("G", "pp", "s", 4)...
+    pin("B", "pp", "s", 3) pin("D", "pp", "s", 4) pin("B", "pp", "s", 4) pin("G", "pp", "s", 4)...
+    pin("B", "pp", "s", 3) pin("D", "pp", "s", 4) pin("B", "pp", "s", 4) pin("G", "pp", "s", 4)...
+    pin("B", "pp", "s", 3) pin("D", "pp", "s", 4) pin("B", "pp", "s", 4) pin("G", "pp", "s", 4)... % 2
+    pin("B", "pp", "s", 3) pin("D", "pp", "s", 4) pin("B", "pp", "s", 4) pin("G", "pp", "s", 4)...
+    pin("B", "pp", "s", 3) pin("D", "pp", "s", 4) pin("B", "pp", "s", 4) pin("G", "pp", "s", 4)...
+    pin("B", "pp", "s", 3) pin("D", "pp", "s", 4) pin("B", "pp", "s", 4) pin("G", "pp", "s", 4)...
+    pin("B", "pp", "s", 3) pin("D", "pp", "s", 4) pin("B", "pp", "s", 4) pin("G", "pp", "s", 4)...
+    pin("B", "pp", "s", 3) pin("D", "pp", "s", 4) pin("B", "pp", "s", 4) pin("G", "pp", "s", 4)...
+    pin("C", "pp", "s", 4) pin("E", "pp", "s", 4) pin("C", "pp", "s", 5) pin("A", "pp", "s", 4)... % 3
+    pin("C", "pp", "s", 4) pin("E", "pp", "s", 4) pin("C", "pp", "s", 5) pin("A", "pp", "s", 4)...
+    pin("C", "pp", "s", 4) pin("E", "pp", "s", 4) pin("C", "pp", "s", 5) pin("A", "pp", "s", 4)...
+    pin("C", "pp", "s", 4) pin("E", "pp", "s", 4) pin("C", "pp", "s", 5) pin("A", "pp", "s", 4)...
+    pin("C", "pp", "s", 4) pin("E", "pp", "s", 4) pin("C", "pp", "s", 5) pin("A", "pp", "s", 4)...
+    pin("C", "pp", "s", 4) pin("E", "pp", "s", 4) pin("C", "pp", "s", 5) pin("A", "pp", "s", 4)...
+    pin("C", "pp", "s", 4) pin("E", "pp", "s", 4) pin("C", "pp", "s", 5) pin("A", "pp", "s", 4)... % 4
+    pin("C", "pp", "s", 4) pin("E", "pp", "s", 4) pin("C", "pp", "s", 5) pin("A", "pp", "s", 4)...
+    pin("C", "pp", "s", 4) pin("E", "pp", "s", 4) pin("C", "pp", "s", 5) pin("A", "pp", "s", 4)...
+    pin("C", "pp", "s", 4) pin("E", "pp", "s", 4) pin("C", "pp", "s", 5) pin("A", "pp", "s", 4)...
+    pin("C", "pp", "s", 4) pin("E", "pp", "s", 4) pin("C", "pp", "s", 5) pin("A", "pp", "s", 4)...
+    pin("C", "pp", "s", 4) pin("E", "pp", "s", 4) pin("C", "pp", "s", 5) pin("A", "pp", "s", 4)...
+    pin("B", "pp", "s", 3) pin("D", "pp", "s", 4) pin("B", "pp", "s", 4) pin("G", "pp", "s", 4)... % 5
+    pin("B", "pp", "s", 3) pin("D", "pp", "s", 4) pin("B", "pp", "s", 4) pin("G", "pp", "s", 4)...
+    pin("B", "pp", "s", 3) pin("D", "pp", "s", 4) pin("B", "pp", "s", 4) pin("G", "pp", "s", 4)...
+    pin("B", "pp", "s", 3) pin("D", "pp", "s", 4) pin("B", "pp", "s", 4) pin("G", "pp", "s", 4)...
+    pin("B", "pp", "s", 3) pin("D", "pp", "s", 4) pin("B", "pp", "s", 4) pin("G", "pp", "s", 4)...
+    pin("B", "pp", "s", 3) pin("D", "pp", "s", 4) pin("B", "pp", "s", 4) pin("G", "pp", "s", 4)...
+    pin("B", "pp", "s", 3) pin("D", "pp", "s", 4) pin("B", "pp", "s", 4) pin("G", "pp", "s", 4)... % 6
+    pin("B", "pp", "s", 3) pin("D", "pp", "s", 4) pin("B", "pp", "s", 4) pin("G", "pp", "s", 4)...
+    pin("B", "pp", "s", 3) pin("D", "pp", "s", 4) pin("B", "pp", "s", 4) pin("G", "pp", "s", 4)...
+    pin("B", "pp", "s", 3) pin("D", "pp", "s", 4) pin("B", "pp", "s", 4) pin("G", "pp", "s", 4)...
+    pin("B", "pp", "s", 3) pin("D", "pp", "s", 4) pin("B", "pp", "s", 4) pin("G", "pp", "s", 4)...
+    pin("B", "pp", "s", 3) pin("D", "pp", "s", 4) pin("B", "pp", "s", 4) pin("G", "pp", "s", 4)...
+    pin("Cs", "pp", "s", 4) pin("E", "pp", "s", 4) pin("Cs", "pp", "s", 5) pin("As", "pp", "s", 4)... % 7
+    pin("Cs", "pp", "s", 4) pin("E", "pp", "s", 4) pin("Cs", "pp", "s", 5) pin("As", "pp", "s", 4)...
+    pin("Cs", "pp", "s", 4) pin("E", "pp", "s", 4) pin("Cs", "pp", "s", 5) pin("As", "pp", "s", 4)...
+    pin("Cs", "pp", "s", 4) pin("E", "pp", "s", 4) pin("Cs", "pp", "s", 5) pin("As", "pp", "s", 4)...
+    pin("Cs", "pp", "s", 4) pin("E", "pp", "s", 4) pin("Cs", "pp", "s", 5) pin("As", "pp", "s", 4)...
+    pin("Cs", "pp", "s", 4) pin("E", "pp", "s", 4) pin("Cs", "pp", "s", 5) pin("As", "pp", "s", 4)...
+    pin("B", "pp", "s", 3) pin("D", "pp", "s", 4) pin("B", "pp", "s", 4) pin("Fs", "pp", "s", 4)... % 8
+    pin("B", "pp", "s", 3) pin("D", "pp", "s", 4) pin("B", "pp", "s", 4) pin("Fs", "pp", "s", 4)...
+    pin("B", "pp", "s", 3) pin("D", "pp", "s", 4) pin("B", "pp", "s", 4) pin("Fs", "pp", "s", 4)...
+    pin("As", "pp", "s", 3) pin("E", "pp", "s", 4) pin("As", "pp", "s", 4) pin("Fs", "pp", "s", 4)...
+    pin("As", "pp", "s", 3) pin("E", "pp", "s", 4) pin("As", "pp", "s", 4) pin("Fs", "pp", "s", 4)...
+    pin("As", "pp", "s", 3) pin("E", "pp", "s", 4) pin("As", "pp", "s", 4) pin("Fs", "pp", "s", 4)...
+    pin("D", "pp", "s", 4) pin("Fs", "pp", "s", 4) pin("B", "pp", "s", 4) pin("Fs", "pp", "s", 4)... % 9
+    pin("D", "pp", "s", 4) pin("Fs", "pp", "s", 4) pin("B", "pp", "s", 4) pin("Fs", "pp", "s", 4)...
+    pin("D", "pp", "s", 4) pin("Fs", "pp", "s", 4) pin("B", "pp", "s", 4) pin("Fs", "pp", "s", 4)...
+    pin("D", "pp", "s", 4) pin("Fs", "pp", "s", 4) pin("B", "pp", "s", 4) pin("Fs", "pp", "s", 4)...
+    pin("D", "pp", "s", 4) pin("Fs", "pp", "s", 4) pin("B", "pp", "s", 4) pin("Fs", "pp", "s", 4)...
+    pin("D", "pp", "s", 4) pin("Fs", "pp", "s", 4) pin("B", "pp", "s", 4) pin("Fs", "pp", "s", 4)];
+
+% Piano bass part note vector
+
+pianoBass = [pb("G", "mf", "e", 2) pb("D", "p", "e", 3) pb("G", "p", "e", 3) pb("D", "p", "e", 3) pb("G", "mf", "e", 2) pb("D", "p", "e", 3)...
+    pb("G", "mf", "e", 2) pb("D", "p", "e", 3) pb("G", "p", "e", 3) pb("D", "p", "e", 3) pb("G", "mf", "e", 2) pb("D", "p", "e", 3)...
+    pb("G", "mf", "e", 2) pb("D", "p", "e", 3) pb("G", "p", "e", 3) pb("D", "p", "e", 3) pb("G", "mf", "e", 2) pb("D", "p", "e", 3)...
+    pb("G", "mf", "e", 2) pb("D", "p", "e", 3) pb("G", "p", "e", 3) pb("D", "p", "e", 3) pb("G", "mf", "e", 2) pb("D", "p", "e", 3)...
+    pb("G", "mf", "e", 2) pb("E", "p", "e", 3) pb("A", "p", "e", 3) pb("E", "p", "e", 3) pb("G", "mf", "e", 2) pb("E", "p", "e", 3)...
+    pb("G", "mf", "e", 2) pb("E", "p", "e", 3) pb("A", "p", "e", 3) pb("E", "p", "e", 3) pb("G", "mf", "e", 2) pb("E", "p", "e", 3)...
+    pb("G", "mf", "e", 2) pb("E", "p", "e", 3) pb("A", "p", "e", 3) pb("E", "p", "e", 3) pb("G", "mf", "e", 2) pb("E", "p", "e", 3)...
+    pb("G", "mf", "e", 2) pb("E", "p", "e", 3) pb("A", "p", "e", 3) pb("E", "p", "e", 3) pb("G", "mf", "e", 2) pb("E", "p", "e", 3)...
+    pb("G", "mf", "e", 2) pb("D", "p", "e", 3) pb("G", "p", "e", 3) pb("D", "p", "e", 3) pb("G", "mf", "e", 2) pb("D", "p", "e", 3)...
+    pb("G", "mf", "e", 2) pb("D", "p", "e", 3) pb("G", "p", "e", 3) pb("D", "p", "e", 3) pb("G", "mf", "e", 2) pb("D", "p", "e", 3)...
+    pb("G", "mf", "e", 2) pb("D", "p", "e", 3) pb("G", "p", "e", 3) pb("D", "p", "e", 3) pb("G", "mf", "e", 2) pb("D", "p", "e", 3)...
+    pb("G", "mf", "e", 2) pb("D", "p", "e", 3) pb("G", "p", "e", 3) pb("D", "p", "e", 3) pb("G", "mf", "e", 2) pb("D", "p", "e", 3)...
+    pb("G", "mf", "e", 2) pb("E", "p", "e", 3) pb("As", "p", "e", 3) pb("E", "p", "e", 3) pb("G", "mf", "e", 2) pb("E", "p", "e", 3)...
+    pb("G", "mf", "e", 2) pb("E", "p", "e", 3) pb("As", "p", "e", 3) pb("E", "p", "e", 3) pb("G", "mf", "e", 2) pb("E", "p", "e", 3)...
+    pb("Fs", "mf", "e", 2) pb("D", "p", "e", 3) pb("Fs", "p", "e", 3) pb("D", "p", "e", 3) pb("Fs", "mf", "e", 2) pb("D", "p", "e", 3)...
+    pb("Fs", "mf", "e", 2) pb("Cs", "p", "e", 3) pb("Fs", "p", "e", 3) pb("Cs", "p", "e", 3) pb("Fs", "mf", "e", 2) pb("Cs", "p", "e", 3)...
+    pb("B", "mf", "e", 2) pb("Fs", "p", "e", 3) pb("B", "p", "e", 3) pb("Fs", "p", "e", 3) pb("B", "mf", "e", 2) pb("Fs", "p", "e", 3)...
+    pb("B", "mf", "e", 2) pb("Fs", "p", "e", 3) pb("B", "p", "e", 3) pb("Fs", "p", "e", 3) pb("B", "mf", "e", 2) pb("Fs", "p", "e", 3)];
+
+% Cello part note vector
+
+cello = [clo("G", "rest", "w", 4) clo("Fs", "rest", "q", 4) clo("B", "rest", "q", 3)... % 1
+    clo("G", "mf", "q", 4) clo("Fs", "mf", "q", 4) clo("B", "mf", "q", 3)...% 2
+    clo("E", "mf", "q", 4) clo("D", "mf", "q", 4) clo("G", "mf", "q", 3)... 
+    clo("A", "mf", "he", 3) clo("B", "mf", "e", 3) clo("C", "mf", "h", 4) clo("C", "rest", "q", 4)... % 3
+    clo("E", "mf", "h", 3) clo("Fs", "mf", "e", 3) clo("G", "mf", "e", 3)... % 4
+    clo("A", "mf", "e", 3) clo("B", "mf", "e", 3) clo("C", "mp", "e", 4) clo("D", "mp", "e", 4) clo("E", "p", "e", 4) clo("Fs", "p", "e", 4)...
+    clo("B", "pp", "w", 4) clo("D", "rest", "h", 4)... % 5
+    clo("G", "mf", "q", 4) clo("Fs", "mf", "q", 4) clo("B", "mf", "q", 3)... % 6
+    clo("E", "mf", "q", 4) clo("D", "mf", "q", 4) clo("G", "mf", "q", 3)...
+    clo("As", "mf", "he", 3) clo("B", "mf", "e", 3) clo("Cs", "mf", "h", 4) clo("B", "rest", "q", 3)...
+    clo("Fs", "mf", "qe", 3) clo("Gs", "mf", "e", 3) clo("As", "mf", "e", 3) clo("B", "mf", "e", 3)...
+    clo("Cs", "mf", "e", 4) clo("D", "mf", "e", 4) clo("E", "mp", "e", 4) clo("Fs", "mp", "e", 4) clo("Gs", "p", "e", 4) clo("As", "p", "e", 4)...
+    clo("D", "ppp", "w", 5) clo("D", "rest", "h", 5)]; % 7
+
+% Example of 0.25 second echo effect - chose not to implement
+
+% echo = 0.25 * [zeros(1, 0.25 * 8000) notes];
+% notes = [notes zeros(1, 0.25 * 8000)]
+% final = notes + echo;
+
+% Example of potential use of white noise wave - this could work as a
+% series of high hats
+
+% noise = [wn("ppp", "s") wn("rest", "s") wn("ppp", "s") wn("rest", "s") wn("ppp", "s") wn("rest", "s") wn("ppp", "s") wn("rest", "s") wn("ppp", "s") wn("rest", "s")];
+
+% Adding reverb to the cello and piano treble parts using a built-in MATLAB reverb effect
+reverb = reverberator;
+cello = reverb(transpose(cello));
+pianoTreble = reverb(transpose(pianoTreble));
+% disp(length(pianoBass) + " " + length(pianoTreble) + " " + length(cello))
+
+pianoTreble1 = transpose(pianoTreble);
+cello1 = transpose(cello);
+
+pianoTreble2 = pianoTreble1(1,:);
+cello2 = cello1(1,:);
+
+% Play the final song, normalized by volumne scaled so that the max
+% amplitude is 1
+
+finalSong = pianoBass + pianoTreble2 + cello2;
+finalNorm = finalSong ./ max(finalSong);
+filename = "Glas_The_Swan1.wav";
+audiowrite(filename, finalNorm, 8000);
+soundsc(finalSong, 8000);
+
+
+% Returns note length in seconds depending on the note value and bpm. Acts
+% as an ENUM to return note length.
+
+function dur = noteDuration(noteType)
+    bpm = 80;
+    switch noteType
+        case "s"
+            dur = 15/bpm;
+        case "e"
+            dur = 30/bpm;
+        case "q"
+            dur = 60/bpm;
+        case "qe"
+            dur = 90/bpm;
+        case "h"
+            dur = 120/bpm;
+        case "w"
+            dur = 240/bpm;
+        case "he"
+            dur = 150/bpm;
+        case "hq"
+            dur = 180/bpm;
+    end
+end
+
+% Returns note frequency depending on the note pitch. Returns pitch of
+% notes in the third octive (i.e. C3, D3,...,B3) - octive is adjusted later as part of the
+% individual not functions.
+
+function p = noteFrequency(pitch)
+    switch pitch
+        case "C"
+            p = 261.63;
+        case "Cs"
+            p = 277.18;
+        case "D"
+            p =293.66;
+        case "Ds"
+            p = 311.13;
+        case "E"
+            p = 329.63;
+        case "F"
+            p = 349.23;
+        case "Fs"
+            p = 369.99;
+        case "G"
+            p = 392;
+        case "Gs"
+            p = 415.3; 
+        case "A"
+            p = 440;
+        case "As" 
+            p = 466.16;
+        case "B"
+            p = 493.88;
+    end
+end
+
+% Returns numeric amplitude of note depending on passed dynamic. Ranges
+% from 0 (rest) to 1 (fff).
+function d = noteDynamic(dynamic)
+    switch dynamic
+        case "rest"
+            d = 0;
+        case "ppp"
+            d= .125;
+        case "pp"
+            d = .25;
+        case "p"
+            d = .375;
+        case "mp"
+            d = .5;
+        case "mf"
+            d = .625;
+        case "f"
+            d = .750;
+        case "ff"
+            d = .875;
+        case "fff"
+            d = 1;
+    end
+end
+
+% Used to generate notes in the piano treble part. Creates more of
+% pluck/pulse-like sound by reducing the attack and decay time lengths.
+% Just consists of the fundamental frequency to create a more pure and less
+% stuffy/nasaly sound.
+
+function n = pin(pitch, dynamic, duration, octive)
+    fs = 8000;
+    t = [0:(1/fs):(noteDuration(duration)-1/fs)];
+    n = noteDynamic(dynamic) * cos(2 * pi * noteFrequency(pitch) * 2^(octive - 4) * t);
+%     n = noteDynamic(dynamic) * cos(2 * pi * (noteFrequency(pitch) + 40 * cos(2 * pi * 15 * t)) * 2^(octive - 3) * t);
+%     n = n + 0.4 * noteDynamic(dynamic) * sin(2 * 2 * pi * noteFrequency(pitch) * 2^(octive - 3) * t);
+%     n = n + 0.2 * noteDynamic(dynamic) * cos(3 * pi * noteFrequency(pitch) * 2^(octive - 3) * t);
+%     n = n + 0.2 * noteDynamic(dynamic) * sawtooth(4 * pi * noteFrequency(pitch) * 2^(octive - 3) * t,1/2);
+%     n = n + 0.05 * noteDynamic(dynamic) * cos(5 * pi * noteFrequency(pitch) * 2^(octive - 3) * t);
+%     n = n + 0.05 * noteDynamic(dynamic) * cos(6 * pi * noteFrequency(pitch) * 2^(octive - 3) * t);
+    
+    A = linspace(0, 1, 0.1*length(t));
+    D = linspace(1, 0.4, 0.1 * length(t));
+    S = linspace(0.4, 0.4, 0.6 * length(t));
+    R = linspace(0.4, 0, 0.2 * length(t));
+    
+    ADSR = [A, D, S, R];
+    n = n.*ADSR;
+    n = n;
+end
+
+% Used to generate notes in the piano bass part. Implements a triangle
+% wave to make lower notes more audible, as my computer speakers could note
+% produce a clear enough sinusoidal bass part.
+
+function n = pb(pitch, dynamic, duration, octive)
+    fs = 8000;
+    t = [0:(1/fs):(noteDuration(duration)-1/fs)];
+    n = noteDynamic(dynamic) * sawtooth(2 * pi * noteFrequency(pitch) * 2^(octive - 4) * t,1/2);
+%     n = noteDynamic(dynamic) * cos(2 * pi * (noteFrequency(pitch) + 40 * cos(2 * pi * 15 * t)) * 2^(octive - 3) * t);
+    n = n + 0.1 * noteDynamic(dynamic) * sin(2 * 2 * pi * noteFrequency(pitch) * 2^(octive - 3) * t);
+    n = n + 0.1 * noteDynamic(dynamic) * cos(3 * pi * noteFrequency(pitch) * 2^(octive - 3) * t);
+%     n = n + 0.2 * noteDynamic(dynamic) * sawtooth(4 * pi * noteFrequency(pitch) * 2^(octive - 3) * t,1/2);
+%     n = n + 0.05 * noteDynamic(dynamic) * cos(5 * pi * noteFrequency(pitch) * 2^(octive - 3) * t);
+%     n = n + 0.05 * noteDynamic(dynamic) * cos(6 * pi * noteFrequency(pitch) * 2^(octive - 3) * t);
+    
+    A = linspace(0, 1, 0.2*length(t));
+    D = linspace(1, 0.6, 0.1 * length(t));
+    S = linspace(0.6, 0.6, 0.5 * length(t));
+    R = linspace(0.6, 0, 0.2 * length(t));
+    
+    ADSR = [A, D, S, R]
+    n = n.*ADSR
+    n = n / 3
+end
+
+% Used to generate notes in the cello part. Implements a triangle
+% wave to make lower notes more audible, as my computer speakers could note
+% produce a clear enough sinusoidal bass part.
+
+function n = clo(pitch, dynamic, duration, octive)
+    fs = 8000;
+    t = [0:(1/fs):(noteDuration(duration)-1/fs)];
+    n = noteDynamic(dynamic) * cos(2 * pi * noteFrequency(pitch) * 2^(octive - 4) * t);
+%     n = noteDynamic(dynamic) * cos(2 * pi * (noteFrequency(pitch) + 40 * cos(2 * pi * 15 * t)) * 2^(octive - 3) * t);
+    n = n + 0.1 * noteDynamic(dynamic) * cos(2 * 2 * pi * noteFrequency(pitch) * 2^(octive - 3) * t);
+    n = n + 0.05 * noteDynamic(dynamic) * cos(3 * 2 * pi * noteFrequency(pitch) * 2^(octive - 3) * t);
+    n = n + 0.02 * noteDynamic(dynamic) * cos(4 * 2 * pi * noteFrequency(pitch) * 2^(octive - 3) * t);
+%     n = n + 0.2 * noteDynamic(dynamic) * cos(5 * 2 * pi * noteFrequency(pitch) * 2^(octive - 3) * t);
+%     n = n + 0.01 * noteDynamic(dynamic) * cos(6 * 2 * pi * noteFrequency(pitch) * 2^(octive - 3) * t);
+%     n = n + 0.45 * noteDynamic(dynamic) * cos(7 * 2 * pi * noteFrequency(pitch) * 2^(octive - 3) * t);
+%     n = n + 0.4 * noteDynamic(dynamic) * cos(8 * 2 * pi * noteFrequency(pitch) * 2^(octive - 3) * t);
+%     n = n + 0.3 * noteDynamic(dynamic) * cos(9 * 2 * pi * noteFrequency(pitch) * 2^(octive - 3) * t);
+%     n = n + 0.1 * noteDynamic(dynamic) * cos(10 * 2 * pi * noteFrequency(pitch) * 2^(octive - 3) * t);
+    
+    A = linspace(0, 1, 0.2*length(t));
+    D = linspace(1, 0.6, 0.1 * length(t));
+    S = linspace(0.6, 0.6, 0.69 * length(t));
+    R = linspace(0.6, 0, 0.01 * length(t));
+    
+    ADSR = [A, D, S, R];
+    
+    if duration == "qe"
+        ADSR = [ADSR, 0.0];
+    end
+    
+    disp(length(ADSR) + " " + length(n));
+    n = n.*ADSR;
+    n = n / (1.8);
+end
+   
+% Produces white noise
+function oh = wn(dynamic, duration)
+    fs = 8000;
+    t = [0:(1/fs):(noteDuration(duration)-1/fs)];
+    oh = noteDynamic(dynamic) * transpose(rand(length(t), 1));
+%     disp(oh)
+end
+
+% Produces square wave
+function wave = sqW(pitch, dynamic, duration, octive)
+    fs = 8000;
+    t = [0:(1/fs):(noteDuration(duration)-1/fs)];
+    wave = noteDynamic(dynamic) * square(2 * pi * noteFrequency(pitch) * 2^(octive - 3) * t);
+end
+
+% Produces saw wave
+function wave = saW(pitch, dynamic, duration, octive)
+    fs = 8000;
+    t = [0:(1/fs):(noteDuration(duration)-1/fs)];
+    wave = noteDynamic(dynamic) * sawtooth(2 * pi * noteFrequency(pitch) * 2^(octive - 3) * t);
+end
+
+% Produces triangle wave
+function wave = trW(pitch, dynamic, duration, octive)
+    fs = 8000;
+    t = [0:(1/fs):(noteDuration(duration)-1/fs)];
+    wave = noteDynamic(dynamic) * sawtooth(2 * pi * noteFrequency(pitch) * 2^(octive - 3) * t,1/2);
+end
+
+% Produces pink noise
+
+function wave = piN(duration)
+    fs = 8000;
+    t = [0:(1/fs):(noteDuration(duration)-1/fs)];
+    wave = pinknoise(noteDuration(duration) * fs)
+end
+    
